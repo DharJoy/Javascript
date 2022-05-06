6e40260a21ec0d42412ce53e2c372077b7f507e8
Joydhar <mr.joydhar.bd@gmail.com>
2022-05-06 12:20:38 +0600

    Deleted old

--- Javascript Array/methods/at.js
+++
@@ -1,7 +0,0 @@
-/** ######################### Array.at() #########################
- * * The at() method takes an integer value and returns the item at that index, allowing for positive and negative integers. Negative integers count back from the last item in the array.
- * *
- */
-
-let arr = [10, 20, 30, 40, 50];
-console.log(arr.flat);
--- Javascript Array/methods/toString.js
+++
@@ -1,9 +0,0 @@
-/** ######################### Array.toString() #########################
- * * The toString() method returns a string with array values separated by commas.
- * * The toString() method does not change the original array.
- */
-let arr = ["Jaylan", "Nella", "Fredy", "Robyn"];
-let stringed =arr.toString();
-console.log(stringed); 
-
-console.log([1,2,3,4,5].toString());
